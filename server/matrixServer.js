f3ad486a934a52138b828e21b802e12c2560a41b
Will Heron <willheron99@gmail.com>
2021-03-23 19:08:22 +0000

    Implemented Footprinting Scaling

--- server/matrixServer.js
+++ server/matrixServer.js
@@ -16,6 +16,7 @@ const fs = require('fs');
 
 // Nominated import of the shared utility functions
 const utils = require('../shared/utils');
+const { request } = require('http');
 
 let packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
@@ -50,6 +51,8 @@ let iteration_count = 0;
 function multiplyBlock(block_a, block_b, results, res_id, node_connection, callback) {
     // Get the (square) size of both blocks
     const block_size = math.size(block_a).valueOf()[0];
+
+    // Perform RPC using the provided node
     node_connection.multiplyMatrices(
         {
             a: {values: block_a.valueOf().flat(), size: block_size},
@@ -61,7 +64,10 @@ function multiplyBlock(block_a, block_b, results, res_id, node_connection, callb
 
             let mat_res = math.matrix(grpc_res.matrix.values);
             mat_res = math.reshape(mat_res, [grpc_res.matrix.size, -1]);
-            console.log(mat_res);
+
+            //* Debug: Log the incoming result from processing node
+            //console.log(mat_res + "\n");
+
             results[res_id] = mat_res;
             multiplyCallback(results, callback);
         }
@@ -70,7 +76,7 @@ function multiplyBlock(block_a, block_b, results, res_id, node_connection, callb
 
 // Multiply a collection of blocks, utilising no footprint/scaling:
 function multiplyBlocks(mat_a, mat_b, callback) {
-    let results = {}
+    let results = {};
     
     // Multiply each block, with no blocking/waiting
     multiplyBlock(mat_a.A, mat_b.A, results, "AE", node_clients[iteration_count % node_clients.length], callback)
@@ -92,14 +98,69 @@ function multiplyBlocks(mat_a, mat_b, callback) {
 }
 
 /*
-function multiplyBlockDeadlined(mat_a, mat_b, deadline, callback) {
+ * This implementation of multiplyBlock will utilise the deadline provided with the request, in order to attempt
+ *      to best meet the deadline. Based on the time taken for the first RPC, the remaining calls will be divided
+ *      across a number of processing nodes (such that they are processed in a queue across a number of nodes) in
+ *      order to attempt to meet the deadline.
+ */
+function multiplyBlocksDeadlined(mat_a, mat_b, deadline, callback) {
+    let results = {};
+    let block_size = math.size(mat_a.A).valueOf()[0];       // Get size of individual block
 
-} */
+    // Perform first block multiplication, in order to get the time-taken
+    node_clients[iteration_count % node_clients.length].multiplyMatrices(
+        {
+            a: {values: mat_a.A.valueOf().flat(), size: block_size},
+            b: {values: mat_b.A.valueOf().flat(), size: block_size}
+        },
+        (grpc_req, grpc_res) => {
+            // RPC callback for processing node - get result then handle 
+            console.log("Multiply Response recieved, block: AE");
 
+            let mat_res = math.matrix(grpc_res.matrix.values);
+            mat_res = math.reshape(mat_res, [grpc_res.matrix.size, -1]);
+            results["AE"] = mat_res;
+
+            // Estimate needed servers to meet deadline
+            let time_taken = Math.max(grpc_res.processTime, 1);     // Minimum process time, considered 1ms
+            console.log("TIME TAKEN FOR AE: " + time_taken);
+            let time_dif = deadline - time_taken;                   // Remaining time to meet deadline
+            let required_servers = Math.ceil(7 / Math.max(Math.floor(time_dif / time_taken), 1) );
+            console.log("REQUIRED SERVERS: " + required_servers);
+
+            // Ensure we will not attempt to reserve more servers than available:
+            // ! This does not yet make use of dynamically reserving currently inactive servers - it uses FIFO on the master list
+            required_servers = Math.min(required_servers, node_clients.length); 
+            let local_count = 0;        // Track the local number of calls made
+            console.log("USING SERVERS: " + required_servers)
+
+            // Subsequentally call to process the remaining 7 blocks, using the existing methods,
+            //      but only calling up to required_servers different unique servers.
+            multiplyBlock(mat_a.B, mat_b.C, results, "BG", node_clients[local_count % required_servers], callback)
+            local_count += 1;
+            multiplyBlock(mat_a.A, mat_b.B, results, "AF", node_clients[local_count % required_servers], callback)
+            local_count += 1;
+            multiplyBlock(mat_a.B, mat_b.D, results, "BH", node_clients[local_count % required_servers], callback)
+            local_count += 1;
+            multiplyBlock(mat_a.C, mat_b.A, results, "CE", node_clients[local_count % required_servers], callback)
+            local_count += 1;
+            multiplyBlock(mat_a.D, mat_b.C, results, "DG", node_clients[local_count % required_servers], callback)
+            local_count += 1;
+            multiplyBlock(mat_a.C, mat_b.B, results, "CF", node_clients[local_count % required_servers], callback)
+            local_count += 1;
+            multiplyBlock(mat_a.D, mat_b.D, results, "DH", node_clients[local_count % required_servers], callback)
+        }
+    );
+    iteration_count += 1;
+}
+
+// Takes an object containing the results of the block multiplications, when all eight permutations are
+//      gathered, this function will finalise processing and forward the result to the original callback provided.
 function multiplyCallback(results, callback) {
     let results_count = Object.keys(results).length;
 
     if (results_count != 8) {
+        // Check that we have all 8 blocks - if not we skip this call
         return;
     }
 
@@ -160,10 +221,17 @@ function multiplyMatrices(call, callback) {
     mat_a = utils.splitArray(mat_a);
     mat_b = utils.splitArray(mat_b);
 
-    // Multiply the collection of blocks together, utilising scaling
-    multiplyBlocks(mat_a, mat_b, callback);
-
-    // TODO: Provide option to use footprinting for scaling
+    // Check what type of operation we want to do (deadlined or not)
+    if (call.request.deadline == 0) {
+        // No deadlining - simply serve result as fast as possible
+        console.log("Processing without deadline...")
+        multiplyBlocks(mat_a, mat_b, callback);
+    }
+    else {
+        // TODO: Provide option to use footprinting for scaling
+        console.log("Processing for deadline: " + deadline + "ms")
+        multiplyBlocksDeadlined(mat_a, mat_b, deadline, callback);
+    }
 }
 
 // * Create the server, and begin listening for requests
